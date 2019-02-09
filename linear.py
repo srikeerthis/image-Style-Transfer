430ccc6d93ea44b4e5e13322c9111e1d9e87d21b
srikeerthis <srikeerthisrinivasan@gmail.com>
2019-02-09 16:23:31 +0530

    Adding files

---
+++ linear.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+x=[1,2,3,4,5,6,7,8,9,10]
+y=[100,200,300,400,500,600,700,800,909,1040]
+
+plt.plot(x,y,color='r')
+plt.xlabel('Square feet in 1k')
+plt.ylabel('cost')
+plt.show()
